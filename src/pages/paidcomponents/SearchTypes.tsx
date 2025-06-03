e58e20e6945707de04fb471d433b6e4edc843204
shashishsoni <shshshsoni2003@tutanota.com>
2025-06-04 02:36:54 +0530

    _blank removed

--- src/pages/paidcomponents/SearchTypes.tsx
+++ src/pages/paidcomponents/SearchTypes.tsx
@@ -192,7 +192,7 @@ export function SearchTypes({ settypeofsearch, selected, typeofsearch }: SearchT
                   <p className="text-gray-400 text-sm mt-2">{tool.description}</p>
                   <button
                     onClick={() => {
-                      window.open(tool.link, "_blank");
+                      window.open(tool.link);
                     }}
                     className="mt-4 w-full border border-white/10 text-teal-300  py-2 rounded-md text-sm font-medium transition-colors"
                   >
